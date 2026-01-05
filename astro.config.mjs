6b7a85a5f88ffba0b19ff14aac1af1aae9934732
Macrhys <mackmondejar@gmail.com>
2026-01-05 08:03:55 +0800

    updated design

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
 
-	devToolbar: {
-    enabled: false,
+  devToolbar: {
+  enabled: false,
+},
+
+  vite: {
+    plugins: [tailwindcss()],
   },
-  
 });
-
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yellow Bus Line Inc.';
+export const SITE_DESCRIPTION = 'Welcome to Yellow Bus Line Inc. - Your trusted partner in safe and reliable transportation services. Explore our website to learn more about our offerings and commitment to excellence.';
